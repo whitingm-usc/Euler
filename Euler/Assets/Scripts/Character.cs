8b957b8ad8fce74fcca1a47e6cd9cdeb685687d2
mattw <matt@wholesalealgorithms.com>
2026-07-11 17:37:46 -0700

    added jump

--- Euler/Assets/Scripts/Character.cs
+++ Euler/Assets/Scripts/Character.cs
@@ -7,18 +7,22 @@ public class Character : MonoBehaviour
 {
     public float m_walkSpeed = 4.0f;
     public float m_turnSpeed = 360.0f;  // degrees per second
+    public float m_jumpSpeed = 10.0f;
+    public float m_gravity = 20.0f;
 
     public class CharInput
     {
         public Vector3 m_direction;
         public float m_facingAngle;
         public bool m_attack;
+        public bool m_jump;
     }
 
     Animator m_anim;
     CharacterController m_char;
     CharInput m_input;
     PlayerInput m_playerInput;
+    Vector3 m_velocity;
 
     // Start is called before the first frame update
     void Start()
@@ -33,12 +37,19 @@ public class Character : MonoBehaviour
     void Update()
     {
         Vector2 move = m_playerInput.actions["Move"].ReadValue<Vector2>();
-
-        // read attack input
-#if false
-        if (Input.GetMouseButtonDown(0))
-            m_input.m_attack = true;
-#endif
+        m_input.m_jump = false;
+        if (m_char.isGrounded)
+        {
+            m_input.m_jump = m_playerInput.actions["Jump"].triggered;
+            if (m_input.m_jump)
+            {
+                m_velocity.y = m_jumpSpeed;
+            }
+            else
+            {
+                m_velocity.y = -0.5f;
+            }
+        }
 
         // convert from camera-space to world-space movement
         Vector3 fwd = Camera.main.transform.forward;
@@ -50,20 +61,20 @@ public class Character : MonoBehaviour
         Vector3 moveWorld = move.y * fwd + move.x * rt;
         m_input.m_direction = moveWorld;
 
-#if false
-        // face the way the camera faces
-        m_input.m_facingAngle = Camera.main.transform.localEulerAngles.y;
-#else
         // face the way we are moving
         float throttle = m_input.m_direction.magnitude;
         if (moveWorld.magnitude > 0.01f)
             m_input.m_facingAngle = Mathf.Atan2(m_input.m_direction.x, m_input.m_direction.z) * Mathf.Rad2Deg;
         else
             m_input.m_facingAngle = transform.localEulerAngles.y;
-#endif
 
+        // gravity
+        m_velocity.y -= m_gravity * Time.deltaTime;
         // move in the direction of the input
-        m_char.SimpleMove(m_input.m_direction * m_walkSpeed);
+        float yVel = m_velocity.y;
+        m_velocity = m_input.m_direction * m_walkSpeed;
+        m_velocity.y = yVel;
+        m_char.Move(m_velocity * Time.deltaTime);
 
         // turn to face the angle in the input
         Vector3 ang = transform.localEulerAngles;
@@ -87,5 +98,11 @@ public class Character : MonoBehaviour
             m_anim.SetTrigger("doAttack");
             m_input.m_attack = false;
         }
+        if (m_input.m_jump)
+        {
+            m_anim.SetTrigger("doJump");
+            m_input.m_jump = false;
+        }
+        m_anim.SetBool("onGround", m_char.isGrounded);
     }
 }
